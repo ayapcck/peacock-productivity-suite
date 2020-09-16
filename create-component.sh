46663bfe9e4767e997939b20d108c122517ad4e6
ayapcck <ayapcck@gmail.com>
2020-09-15 21:08:32 -0700

    PPS-13: Modify create-component script
    Problem:
    Create component script should be able to create a functional or class
    component
    
    Solution:
    * Prompt user if component is a class component
    * Add boilerplate code for chosen option to src/components/<component_name>/index.js
    
    Note:
    N/A

--- create-component.sh
+++ create-component.sh
@@ -4,6 +4,10 @@ echo What is the component called?
 
 read component_name
 
+echo "Is this a class component? (y/n)"
+
+read class_component
+
 # make component directory and nav to it
 mkdir src/components/$component_name
 cd src/components/$component_name
@@ -15,3 +19,33 @@ mkdir test stories
 touch index.js
 touch test/index.test.js
 
+# add content to index.js
+if [ $class_component = y ]; then
+
+cat << EOT >> index.js
+import React, { Component } from 'react';
+
+export default class $component_name extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+}
+EOT
+
+else 
+
+cat << EOT >> index.js
+import React from 'react';
+
+const $component_name = () => (
+    <div></div>
+);
+
+export default $component_name;
+EOT
+
+fi
