877f4e7d780c70aeeb46820652e6877473e271c3
ayapcck <ayapcck@gmail.com>
2020-10-12 02:30:52 -0700

    PPS-50: Fix tests

--- src/components/Navigation/test/Navigation.test.js
+++ src/components/Navigation/test/Navigation.test.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router';
 
+import { AuthUserContext } from '../../../config/session';
 import Navigation from '..';
 
 describe('Navigation', () => {
     it('should render without crashing', () => {
         const div = document.createElement('div');
+        const authUser = {
+            name: 'test',
+            role: 'user',
+        };
         const Container = () => (
-            <MemoryRouter initialEntries={[ '/' ]}>
-                <Navigation />
-            </MemoryRouter>
+            <AuthUserContext.Provider value={authUser}>
+                <MemoryRouter initialEntries={[ '/' ]}>
+                    <Navigation />
+                </MemoryRouter>
+            </AuthUserContext.Provider>
         );
         ReactDOM.render(<Container />, div);
     });
