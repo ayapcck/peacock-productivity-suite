bb1bb45c0cd771fc5f7e0243d6a595af1466a74b
ayapcck <ayapcck@gmail.com>
2020-11-22 21:05:04 -0800

    PPS-62: Create Text atom

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,9 +1,11 @@
 import Button from './Button';
 import Icon from './Icon';
+import Text from './Text';
 import TextBox from './TextBox';
 
 export {
     Button,
     Icon,
+    Text,
     TextBox,
 };
