7e12c09f76f74bd53ac267c22425366d7fe55111
ayapcck <ayapcck@gmail.com>
2021-01-03 17:17:32 -0800

    Hopefully fix tests

--- test/commonTests.js
+++ test/commonTests.js
@@ -1,24 +1,3 @@
-jest.mock('../src/config/firebase', () => {
-    class firebase {
-        // Auth API
-        createUserWithEmailAndPassword = () => jest.fn();
-        sendPasswordResetEmail = () => jest.fn();
-        signInWithEmailAndPassword = () => jest.fn();
-        signOut = () => jest.fn();
-        updatePassword = () => jest.fn();
-
-        // User API
-        role = () => jest.fn();
-        roles = () => jest.fn();
-        user = () => jest.fn();
-        users = () => jest.fn();
-
-        getRole = () => jest.fn();
-        getUser = () => jest.fn();
-    }
-    return new firebase();
-});
-
 import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
